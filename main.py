3d1093c303009b07a2ffd4e7cd7b9f78d606f031
Gyakobo <andygyakobo@gmail.com>
2026-08-15 20:35:59 -0400

    Made a statistics dictionary of all the bigram characters

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import torch
 
+words = open("names.txt", "r").read().splitlines()
+
+min_letters = min(len(w) for w in words)
+max_letters = max(len(w) for w in words)
+
+b = {}
+for word in words[:]:
+    chs = ["<S>"] + list(word) + ["<E>"]
+
+    for ch1, ch2 in zip(chs, chs[1:]):
+        bigram = (ch1, ch2)
+        b[bigram] = (
+            b.get(bigram, 0) + 1
+        )  # Technical b.get(bigram, 0) <=> b[bigram] without the safe guard 0
+
+sorted(b.items(), key=lambda kv: kv[1])
