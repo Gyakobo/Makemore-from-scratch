6f2ba87c9886991a9487f9c44b621784eb0bff6b
Gyakobo <andygyakobo@gmail.com>
2026-08-20 03:28:26 -0400

    Attempting to re-make the periodicity dictionary in a 2D array

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 words = open("names.txt", "r").read().splitlines()
 
-for w in words[:2]:
+
+"""
+Here basically you're storing the information in a dictionary,
+however, it's a bit inconvenient as it's way better to store
+such information in a `tensor` 2D array specifically
+"""
+b = {}
+for w in words:
 
     chs = ["."] + list(w) + ["."]
     for ch1, ch2 in zip(chs, chs[1:]):
-        print(ch1, ch2)
+        bigram = (ch1, ch2)
+        b[bigram] = b.get(bigram, 0) + 1
+
+# sorted(b.items(), key=lambda kv: -kv[1])
+# print(f"{b=}")
+
+"""
+Here is a rendition of the same approach only in a 2D tensor array
+"""
