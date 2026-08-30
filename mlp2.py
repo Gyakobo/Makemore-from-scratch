e77df903f145c9e38b1650f2910b9636c10087cc
Gyakobo <andygyakobo@gmail.com>
2026-08-30 18:21:28 -0400

    Made a histogram representation

--- mlp2.py
+++ mlp2.py
@@ -3,8 +3,6 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import random
 
-from mlp import vocab_size
-
 
 # Let's train a deeper network
 class Linear:
@@ -70,11 +68,48 @@ class Tanh:
         return []
 
 
-block_size = 3  # the context size / block size
+words = open("names.txt", "r").read().splitlines()
+
+# Vocabulary of characters
+chars = sorted(list(set("".join(words))))
+stoi = {c: i + 1 for i, c in enumerate(chars)}
+stoi["."] = 0
+itos = {i: c for c, i in stoi.items()}
+vocab_size = len(
+    stoi
+)  # Size of 'vocabulary'/quantity of all the possible/usable characters
+
+block_size = (
+    3  # context length: how many characters do we take to predict the next one?
+)
+
+
+# build the dataset
+def build_dataset(words):
+    X, Y = [], []
+    for w in words:
+        # print(w)
+        context = [0] * block_size
+        for ch in w + ".":
+            ix = stoi[ch]
+            X.append(context)
+            Y.append(ix)
+            # print("".join(itos[i] for i in context), "--->", itos[ix])
+            context = context[1:] + [ix]  # crop and append
+    X = torch.tensor(X)  # (32, 3)
+    Y = torch.tensor(Y)  # (32, )
+    print(X.shape, Y.shape)
+    return X, Y
+
+
 n_embd = 10  # the dimensionality of the character embedding vectors
 n_hidden = 100  # the number of neurons in the hidden layer of the MLP
+n1 = int(0.8 * len(words))
+n2 = int(0.9 * len(words))
+Xtr, Ytr = build_dataset(words[:n1])  # Training split (80%)
+Xdev, Ydev = build_dataset(words[n1:n2])  # Dev/Validation split (10%)
+Xte, Yte = build_dataset(words[n2:])  # Testing split (10%)
 g = torch.Generator().manual_seed(2147483647)  # for reproducibility
-vocab_size = 27
 
 C = torch.randn((vocab_size, n_embd), generator=g)
 layers = [
@@ -141,3 +176,27 @@ for i in range(max_steps):
     lossi.append(loss.log10().item())
 
     break  # AFTER_DEBUG: would take out obviously to run full optimization
+
+# Visualize histograms
+plt.figure(figsize=(20, 4))  # width and height of the plot
+legends = []
+for i, layer in enumerate(layers[:-1]):  # note: exclude the output layer
+    if isinstance(layer, Tanh):
+        t = layer.out
+        print(
+            "layer %d (%10s): mean %+.2f, std %.2f, saturated: %.2f%%"
+            % (
+                i,
+                layer.__class__,
+                t.mean(),
+                t.std(),
+                (t.abs() > 0.97).float().mean() * 100,
+            )
+        )
+        hy, hx = torch.histogram(t, density=True)
+        plt.plot(hx[:-1].detach(), hy.detach())
+        legends.append(f"layer {i} ({layer.__class__.__name__})")
+plt.legend(legends)
+plt.title("activation distribution")
+
+plt.show()
