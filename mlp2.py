72144d1c758d5780917cb1305fc4df7e5ea9216d
Gyakobo <andygyakobo@gmail.com>
2026-08-30 19:03:24 -0400

    Finalized and fixed all the layer inputs

--- mlp2.py
+++ mlp2.py
@@ -115,28 +115,28 @@ g = torch.Generator().manual_seed(2147483647)  # for reproducibility
 
 C = torch.randn((vocab_size, n_embd), generator=g)
 layers = [
-    Linear(n_embd * block_size, n_hidden),
+    Linear(n_embd * block_size, n_hidden, bias=False),
     BatchNorm1d(n_hidden),
     Tanh(),
-    Linear(n_hidden, n_hidden),
+    Linear(n_hidden, n_hidden, bias=False),
     BatchNorm1d(n_hidden),
     Tanh(),
-    Linear(n_hidden, n_hidden),
+    Linear(n_hidden, n_hidden, bias=False),
     BatchNorm1d(n_hidden),
     Tanh(),
-    Linear(n_hidden, n_hidden),
+    Linear(n_hidden, n_hidden, bias=False),
     BatchNorm1d(n_hidden),
     Tanh(),
-    Linear(n_hidden, n_hidden),
+    Linear(n_hidden, n_hidden, bias=False),
     BatchNorm1d(n_hidden),
     Tanh(),
-    Linear(n_hidden, vocab_size),
-    BatchNorm1d(n_hidden),
+    Linear(n_hidden, vocab_size, bias=False),
+    BatchNorm1d(vocab_size),
 ]
 
 with torch.no_grad():
     # last layer: make less confident
-    layers[-1].weight *= 0.1
+    layers[-1].gamma *= 0.1
 
     # all other layers: apply gain
     for layer in layers[:-1]:
