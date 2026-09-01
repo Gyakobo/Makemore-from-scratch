ca49e10c12533f6d9556eed91192814812a42afa
Gyakobo <andygyakobo@gmail.com>
2026-08-31 23:28:00 -0400

    Build the dataset

--- backpropagation.py
+++ backpropagation.py
@@ -1,4 +1,49 @@
 import torch
+import random
+import torch.nn.functional as F
+import matplotlib as plt
+
+from mlp import vocab_size
+
+words = open("names.txt", "r").read().splitlines()
+
+# build the vocabulary of characters and mappings to/from integers
+chars = sorted(list(set("".join(words))))
+stoi = {s: i + 1 for i, s in enumerate(chars)}
+stoi["."] = 0
+itos = {i: s for s, i in stoi.items()}
+vocab_size = len(itos)
+
+
+# build the dataset
+block_size = (
+    3  # context length: how many characters do we take to predict the next one?
+)
+
+
+def build_dataset(words):
+    X, Y = [], []
+
+    for w in words:
+        context = [0] * block_size
+        for ch in w + ".":
+            ix = stoi[ch]
+            X.append(context)
+            Y.append(ix)
+            context = context[1:] + [ix]  # crop and append
+    X = torch.tensor(X)
+    Y = torch.tensor(Y)
+    print(X.shape, Y.shape)
+
+
+random.seed(42)
+random.shuffle(words)
+n1 = int(0.8 * len(words))
+n2 = int(0.9 * len(words))
+
+Xtr, Ytr = build_dataset(words[:n1])  # 80%
+Xdev, Ydev = build_dataset(words[n1:n2])  # 10%
+Xte, Yte = build_dataset(words[n2:])  # 10%
 
 
 # utility function we will use later when comparing manual gradients to PyTorch gradients
