d8117e934b9e0ec25c8c54116eb48f7119cf3203
Gyakobo <andygyakobo@gmail.com>
2026-09-01 00:16:34 -0400

    re-wrote the forward pass

--- backpropagation.py
+++ backpropagation.py
@@ -45,6 +45,8 @@ Xtr, Ytr = build_dataset(words[:n1])  # 80%
 Xdev, Ydev = build_dataset(words[n1:n2])  # 10%
 Xte, Yte = build_dataset(words[n2:])  # 10%
 
+# ok boilerplate done, now we get to the action:
+
 
 # utility function we will use later when comparing manual gradients to PyTorch gradients
 def cmp(s, dt, t):
@@ -54,3 +56,81 @@ def cmp(s, dt, t):
     print(
         f"{s:15s} | exact: {str(ex):5s} | approximate: {str(app):5s} | maxdiff: {maxdiff}"
     )
+
+
+n_embd = 10  # the dimensionality of the character embedding vectors
+n_hidden = 64  # the number of neurons in the hidden layer of the MLP
+
+g = torch.Generator().manual_seed(2147483647)  # for reproducibility
+C = torch.randn((vocab_size, n_embd), generator=g)
+
+# Layer 1
+W1 = (
+    torch.randn((n_embd * block_size, n_hidden), generator=g)
+    * (5 / 3)
+    / ((n_embd * block_size) ** 0.5)
+)
+b1 = (
+    torch.randn(n_hidden, generator=g) * 0.1
+)  # using b1 just for fun, it's useless because of BN(bnbias)
+
+# Layer 2
+W2 = torch.randn((n_hidden, vocab_size), generator=g) * 0.1
+b2 = torch.randn(vocab_size, generator=g) * 0.1
+
+# BatchNorm parameters
+bngain = torch.randn((1, n_hidden)) * 0.1 + 1.0
+bnbias = torch.randn((1, n_hidden)) * 0.1
+
+# Note: I'm initializing many of these parameters in non-standard ways
+# because sometimes initializing with e.g. all zeros could mask an incorrect
+# implementation of the backward pass.
+
+parameters = [C, W1, b1, W2, b2, bngain, bnbias]
+print(sum(p.element() for p in parameters))  # number of parameters in total
+for p in parameters:
+    p.requires_grad = True
+
+n = batch_size = 32  # just a short and convenient variable
+# construct a minibatch
+ix = torch.randint(0, Xtr.shape[0], (batch_size,), generator=g)
+Xb, Yb = Xtr[ix], Ytr[ix]  # batch X, Y
+
+
+# forward pass, "chunkated" into smaller steps that are possible to backward one at a time
+emb = C[Xb]  # embed the characters into vectors
+embcat = emb.view(emb.shape[0], -1)  # concatenate the vectors
+
+# Linear Layer 1
+hprebn = embcat @ W1 + b1  # hidden layer pre-activation
+
+# BatchNorm layer
+bnmeani = 1 / n * hprebn.sum(0, keepdim=True)
+bndiff = hprebn - bnmeani
+bndiff2 = bndiff**2
+bnvar = (
+    1 / (n - 1) * (bndiff2).sum(0, keepdim=True)
+)  # note: Bessel's correction correction (dividing by n-1, not n)
+
+bnvar_inv = (bnvar + 1e-5) ** -0.5
+bnraw = bndiff * bnvar_inv
+hpreact = bngain * bnraw + bnbias
+
+# Non-linearity (activation)
+h = torch.tanh(hpreact)  # hidden layer
+
+# Linear Layer 2
+logits = h @ W2 + b2  # output layer
+
+# cross entropy loss (same as F.cross_entropy(logits, Yb))
+logit_maxes = logits.max(1, keepdim=True).values
+norm_logits = logits - logit_maxes  # subtract max for numerical stability
+counts = norm_logits.exp()
+counts_sum = counts.sum(1, keepdim=True)
+counts_sum_inv = (
+    counts_sum**-1
+)  # if I use (1.0/counts_sum) instead then I cant' get backprop to be bit exact...
+
+probs = counts * counts_sum_inv
+logprobs = probs.log()
+loss = -logprobs[range(n), Yb].mean()
